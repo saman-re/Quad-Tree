b4f287093ced939aefead25ae4917ac41812b1ec
saman-re <saman.rezapoor1379@yahoo.com>
2021-07-09 14:11:25 +0430

    point class added

---
+++ src/Point.java
@@ -0,0 +1,14 @@
+public class Point {
+
+    double x, y;
+    String parentAdd;
+
+    public void setParentAdd(String parentAdd) {
+        this.parentAdd = parentAdd;
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
