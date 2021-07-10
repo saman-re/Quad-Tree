f9167b0b6a027802b4bdd63ed9c553b464a5510b
saman-re <saman.rezapoor1379@yahoo.com>
2021-07-10 20:11:03 +0430

    result class added to the project

--- src/MinDistanceResult.java
+++ src/MinDistanceResult.java
@@ -1,2 +1,23 @@
-public class MindistanceResult {
+public class MinDistanceResult {
+    private double distance;
+    private Point minPoint;
+
+    public MinDistanceResult() {
+    }
+
+    public void setMinDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void setMinPoint(Point minPoint) {
+        this.minPoint = minPoint;
+    }
+
+    public double getMinDistance() {
+        return distance;
+    }
+
+    public Point getMinPoint() {
+        return minPoint;
+    }
 }
